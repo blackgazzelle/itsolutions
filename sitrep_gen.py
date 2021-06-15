9f073508108ea5d774d27aa60085cd22ee0b6098
BlackGazzelle <55161328+blackgazzelle@users.noreply.github.com>
2021-06-15 10:25:12 -0400

    Added validation messages and marked non sitrep emails as read

--- sitrep_gen.py
+++ sitrep_gen.py
@@ -13,7 +13,7 @@ def send_email(email_recipient,
                email_message,
                attachment_location = ''):
 
-    email_sender = 'sitrcreator@outlook.com'
+    email_sender = 'sitcreator@outlook.com'
 
     msg = MIMEMultipart()
     msg['From'] = email_sender
@@ -48,13 +48,24 @@ def send_email(email_recipient,
 if __name__ == "__main__":
     for i in range(10):
         #Incident, date, location, offender, command, rank, age, race, gender, victim
-        incident = ["DOMESTIC ABUSE", "RACE DISCRIMINATION", "GENDER DISCRIMINATION", "SEXUAL ASSAULT", "VEHICULAR ACCIDENT"]
+        incidents = ["DOMESTIC ABUSE", "RACE DISCRIMINATION", "GENDER DISCRIMINATION", "SEXUAL ASSAULT", "VEHICULAR ACCIDENT"]
         commands = ["New York Yankees", "San Antonio Spurs", "Dallas Cowboys", "University of Texas Longhorns"]
-        gender = ["MALE", "FEMALE"]
-        race = ["WITHHELD", "OTHER", "CAUCASIAN", "AFRICAN-AMERICAN", "ASIAN"]
-        person = ["E-1", "E-2", "E-3", "E-4", "E-5", "E-6", "E-7", "E-8", "E-9", "O-1", "O-2", "O-3", "O-3", "O-4", "O-5", "O-6", "O-7", "O-8", "O-9", "CIV"]
+        genders = ["MALE", "FEMALE"]
+        races = ["WITHHELD", "OTHER", "CAUCASIAN", "AFRICAN-AMERICAN", "ASIAN"]
+        persons = ["E-1", "E-2", "E-3", "E-4", "E-5", "E-6", "E-7", "E-8", "E-9", "O-1", "O-2", "O-3", "O-3", "O-4", "O-5", "O-6", "O-7", "O-8", "O-9", "CIV"]
         adeo = ["YES", "NO"]
+        
+        # Generate the random data for each choice
         d = datetime.now()
-        message = f"1. INCIDENT: {random.choices(incident)[0]}\n2. DATE OF INCIDENT: {d}\n3. LOCATION OF INCIDENT: {random.choices(commands)[0]}\n4. OFFENDER: {random.choices(person)[0]}\n5. COMMAND: {random.choices(commands)[0]}\n6. GENDER: {random.choices(gender)[0]}\n7. AGE: {random.randrange(18, 55)}\n8. RACE: {random.choices(race)[0]}\n9. ALCOHOL: {random.choices(adeo)[0]}, DRUGS: {random.choices(adeo)[0]}"
+        incident = random.choices(incidents)[0]
+        command = random.choices(commands)[0]
+        person = random.choices(persons)[0]
+        gender = random.choices(genders)[0]
+        race = random.choices(races)[0]
+        alc = random.choices(adeo)[0]
+        drugs = random.choices(adeo)[0]
+
+        # Build the message from that data
+        message = f"1. INCIDENT: {incident}\n2. DATE OF INCIDENT: {d}\n3. LOCATION OF INCIDENT: {command}\n4. OFFENDER: {person}\n5. COMMAND: {command}\n6. GENDER: {gender}\n7. AGE: {random.randrange(18, 55)}\n8. RACE: {race}\n9. ALCOHOL: {alc}, DRUGS: {drugs} 10. SUMMARY/BRIEF DESCRIPTION OF INCIDENT: Incident occured on {d} at {command} with {person}"
         send_email("sitreceiver@outlook.com", "sitrep", message, "")
-        time.sleep(10))
+        time.sleep(10)
